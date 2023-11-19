af2524f07851230b64defa5640d5113c2d7b72fe
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-11-19 21:59:48 +0800

    fixed Date and Time type Column

--- sql/TableCreation.sql
+++ sql/TableCreation.sql
@@ -3,10 +3,10 @@ CREATE TABLE ppe_log (
     id              SERIAL PRIMARY KEY,
     photoName       VARCHAR(255) NOT NULL,
     photoURL        VARCHAR(255) NOT NULL,
-    dateAndTime     DATETIME NOT NULL,
+    dateAndTime     TIMESTAMP NOT NULL,
     apronCount      INT NOT NULL,
     bunnysuitCount  INT NOT NULL,
-    gasmaskCount    INT NOT NULL,
+    maskCount       INT NOT NULL,
     glovesCount     INT NOT NULL,
     gogglesCount    INT NOT NULL,
     headcapCount    INT NOT NULL
