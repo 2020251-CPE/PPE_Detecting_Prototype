6a463b8100363e24785c51579678e22ac7ca773e
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-22 19:33:04 +0800

    custom file for custom made Exceptions

---
+++ customExcept.py
@@ -0,0 +1,5 @@
+
+class UnequalArrayLengthException(Exception):
+    def __init__(self, message="Arrays must have the same length"):
+        self.message = message
+        super().__init__(self.message)
