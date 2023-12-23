3bf2da5249f4601ba543b6116280cf4ece706a7a
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-23 23:55:32 +0800

    Created Toggle Button Component Frontend for Detected Objects

---
+++ main/src/components/FilterButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="btn" @click="toggleButton" :class="{ 'btn-secondary': isActive, 'btn-dark': !isActive }">
+      {{ buttonText }}
+    </button>
+    <p v-if="isActive" class="mt-2">Button is active!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'ToggleButton',
+  props: {
+    buttonText: {
+      type: String,
+      default: 'Toggle'
+    }
+  },
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    toggleButton() {
+      this.isActive = !this.isActive;
+    }
+  },
+};
+</script>
+
+<style scoped>
+/* Scoped styles specific to ToggleButton.vue */
+</style>
