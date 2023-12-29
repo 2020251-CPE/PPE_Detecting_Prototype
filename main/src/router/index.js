2737489469675246518fe0d75f2f152a0bd1614b
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-29 21:49:24 +0800

    organized routes + added wildcard and root redirect

--- main/src/router/index.js
+++ main/src/router/index.js
@@ -1,20 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory} from 'vue-router'
 import Ping from '../components/Ping.vue'
 import Logs from '../components/Logs.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/ping',
-      name: 'ping',
-      component: Ping
-    },
-    {
-      path:'/logs',
-      name:'logs',
-      component:Logs
-    }
+    { path:'/', redirect:'/logs' },
+    { path: '/ping', name: 'ping', component: Ping },
+    { path:'/logs', name:'logs', component:Logs },
+    { path:'/:pathMatch(.*)*', redirect:'/logs'}
   ]
 })
 
