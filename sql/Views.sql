67c735b4a16a37c328ea3496e73a590a32b08627
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-30 22:34:30 +0800

    Created VIEW for getting Columns of ppe_log

--- sql/Views.sql
+++ sql/Views.sql
@@ -18,3 +18,15 @@ WHERE dateAndTime >= CURRENT_TIMESTAMP::date
   AND 0 IN (apronCount, bunnysuitCount, maskCount, glovesCount, gogglesCount, headcapCount)
 ;
 
+CREATE VIEW allColumns AS 
+SELECT column_name, data_type
+FROM (
+    SELECT column_name, data_type, 
+           ROW_NUMBER() OVER () AS row_num
+    FROM information_schema.columns
+    WHERE table_schema = 'public' AND table_name = 'ppe_log'
+) AS subquery
+ORDER BY CASE
+    WHEN row_num = 11 THEN 4
+    ELSE row_num
+END;
